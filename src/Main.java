c17f5714ed730e2666b1910394ec8d4a01fb5829
VadimMiVaRi <tulimaku@googlemail.com>
2021-05-29 19:18:25 +0300

    add new users and new method

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Object user = new Object("Max", 34, 12, 1699);
+        Object user1 = new Object("Felix", 12, 2, 123);
+        Object user2 = new Object("Lola", 22, 5, 444);
+        Object user3 = new Object("Zen", 18, 8, 7985);
+
 
         List<Object> list = new ArrayList<>();
-        list.add(2);
-        list.add(7);
-        list.add(8);
-        list.add(84);
-        list.add(0, 14);
-        list.set(1, 6);
-        list.remove(4);
+        list.add(user);
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.remove(1);
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
--- src/Object.java
+++ src/Object.java
@@ -1,4 +1,56 @@
 public class Object {
 
-     private
+     private String name;
+     private int year;
+     private int exp;
+     private int pass;
+
+     public Object(String name, int year, int exp, int pass) {
+          this.name = name;
+          this.year = year;
+          this.exp = exp;
+          this.pass = pass;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public int getYear(int i) {
+          return year;
+     }
+
+     public void setYear(int year) {
+          this.year = year;
+     }
+
+     public int getExp() {
+          return exp;
+     }
+
+     public void setExp(int exp) {
+          this.exp = exp;
+     }
+
+     public int getPass() {
+          return pass;
+     }
+
+     public void setPass(int pass) {
+          this.pass = pass;
+     }
+
+     @Override
+     public String toString() {
+          return "Object{" +
+                  "name='" + name + '\'' +
+                  ", year=" + year +
+                  ", exp=" + exp +
+                  ", pass=" + pass +
+                  '}';
+     }
 }
